f6badae00c2289baf6517d6df733ef8266fdd198
Sahasragoud <146707817+Sahasragoud@users.noreply.github.com>
2026-06-04 15:51:40 +0530

    Add form submission function with debugging

---
+++ JAVASCRIPT/Exercise13.js
@@ -0,0 +1,15 @@
+// 13. Debugging and Testing
+function submitForm() {
+
+    let user = {
+        name: "Rahul"
+    };
+
+    console.log("Form Submission Started");
+
+    debugger;
+
+    console.log("Payload:", user);
+}
+
+submitForm();
