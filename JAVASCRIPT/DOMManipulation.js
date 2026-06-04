73ad16ba77c910f4ce530fc212e51e0916d14d12
Sahasragoud <146707817+Sahasragoud@users.noreply.github.com>
2026-06-04 15:42:28 +0530

    Add DOM manipulation for music festival card

---
+++ JAVASCRIPT/DOMManipulation.js
@@ -0,0 +1,11 @@
+//DOM Manipulation
+const container = document.querySelector("#events");
+
+const card = document.createElement("div");
+card.textContent = "Music Festival";
+
+container.appendChild(card);
+
+function register() {
+    card.textContent = "Registered Successfully";
+}
