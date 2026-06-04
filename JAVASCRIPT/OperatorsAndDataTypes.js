d91a5ef2c6659a02b191d0395fb5d196356ecc9f
Sahasragoud <146707817+Sahasragoud@users.noreply.github.com>
2026-06-04 15:38:02 +0530

    Create OperatorsAndDataTypes.js

---
+++ JAVASCRIPT/OperatorsAndDataTypes.js
@@ -0,0 +1,9 @@
+// 2. Syntax, Data Types, and Operators
+const eventName = "Music Festival";
+const eventDate = "10-06-2026";
+let seats = 50;
+
+console.log(`Event: ${eventName}, Date: ${eventDate}, Seats: ${seats}`);
+
+seats--;
+console.log("Seats after registration:", seats);
